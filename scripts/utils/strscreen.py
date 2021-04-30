cd615370296e220860da6431611f707341aac4b5
Bruce Ferris <bferris57@bferris.co.uk>
2021-04-30 03:24:40 +0100

    Rationalised Rect and Point

--- scripts/utils/strscreen.py
+++ scripts/utils/strscreen.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 #  coding=utf-8
+from __future__ import print_function
 #===============================================================================
 #
 # Module: strscreen - Class StrScreen implementation
 #
 # Notes:  We've improved over 'curses' by introducing the introducing the
 #         concept of movable named highlights using the sub-class Movable.
-# 
+#
+#         A bit of an explanation about the Rect class...
+#
+#           Rectangles, in general, can either be used to represent areas on a
+#           screen or geometric areas.  The difference is subtle and we won't
+#           explain more here but the ones defined here are intended to be used
+#           for screen manipulations NOT geometrical ones.  As such, only
+#           the concept of 'equality' is implemented and they're normalised.
+#
+#           This means a Rect([0,0],[0,0]) has a 'visable area' of 1 and a
+#           'physical area' of 0 so can be 'filled' and represents the
+#           same thing as Point(0,0).
+#
+#           Same goes for the Point class, it represents an area on the screen.
+#           So it can be filled.  ;-)
+#
+#           If you want geometric rectangles and points, go look somewhere else!
+#
 #===============================================================================
 # Copyright (C) 2021 - Bruce Ferris (befe@bferris.co.uk)
 #===============================================================================
@@ -52,6 +70,13 @@ class Point(object):
       raise InternalError("Point expected two integers (y,x)")
     self.y = y
     self.x = x
+    self.normalise()
+
+  def __eq__(self,that):
+
+    if not isinstance(that,Point): return False
+
+    return True if self.x == that.x and self.y == that.y else False
 
   def __str__(self):
 
@@ -61,9 +86,25 @@ class Point(object):
 
     return str(self)
 
+  def __bool__(self):
+
+    if type(self.y) != int or self.y < 0: return False
+    if type(self.x) != int or self.x < 0: return False
+    
+    return True
+
+  __nonzero__ = __bool__
+
+  def normalise(self):
+
+    if type(self.y) != int or self.y < 0:
+      self.y = 0
+    if type(self.x) != int or self.x < 0:
+      self.x = 0
+    
 class Rect(object):
 
-  def __init__(self,tl,br):
+  def __init__(self,tl=None,br=None):
 
     if (tl != None and not isinstance(tl,Point)) or \
        (br != None and not isinstance(br,Point)):
@@ -74,23 +115,23 @@ class Rect(object):
 
   def __str__(self):
 
-    if self.tl and self.br:
-      return 'Rect([%d,%d]->[%d,%d])'%(self.tl.y,self.tl.x,self.br.y,self.br.x)
+    if isinstance(self.tl,Point):
+      left = '[' + repr(self.tl.y) + ',' + repr(self.tl.x) + ']'
     else:
-      return 'Rect(None->None)'
+      left = 'None'
+    if isinstance(self.bt,Point):
+      right = '[' + repr(self.br.y) + ',' + repr(self.bt.x) + ']'
+    else:
+      right = 'None'
+
+      
+    return 'Rect(' + left + '->' + right + ')'
 
   def __repr__(self):
 
-    return str(self)
+    return 'Rect('+repr(self.tl)+','+repr(self.br) + ')'
 
   def area(self):
-
-    if not self.tl or not self.br:
-      return 0
-
-    return (self.br.y - self.tl.y) * (self.br.x - self.tl.x)
-
-  def visarea(self):
 
     if not self.tl or not self.br:
       return 0
@@ -99,12 +140,18 @@ class Rect(object):
 
   def normalise(self):
 
-    if not self.tl or not self.br:
-      return
+    self.tl.normalise()
+    self.br.normalise()
     tl = Point(min(self.tl.y,self.br.y),min(self.tl.x,self.br.x))
     br = Point(max(self.tl.y,self.br.y),max(self.tl.x,self.br.x))
     self.tl = tl
     self.br = br
+
+  def __bool__(self):
+
+    return bool(self.tl) and bool(self.br)
+
+  __nonzero__ = __bool__
 
 #------------------------------------------------------------------------------
 #
@@ -250,7 +297,7 @@ if __name__ == '__main__':
     scr.addstr(0,maxx-5,msg)
     scr.addstr(0,0,msg)
 
-  if 1:
+  if 0:
 
     r1 = Rect(Point(0,0),Point(0,0))
     print('area(r1)    = %d'%r1.area())
@@ -264,3 +311,28 @@ if __name__ == '__main__':
     print('area(r3)    = %d'%r3.area())
     print('visarea(r3) = %d'%r3.visarea())
    
+  if 1:
+
+    r1 = Rect(Point(0,0),Point(-1,-1))
+    print('r1 = %s'%repr(r1))
+    print('  if r1... %s'%repr(bool(r1)))
+    print('  r1.area() = %d'%r1.area())
+
+    r2 = Rect(Point(0,0),Point(-1,-1))
+    print('r2 = %s'%repr(r2))
+    print('  if r2... %s'%repr(bool(r2)))
+
+    r3 = Rect(Point(0,0),Point(0,0))
+    print('r3 = %s'%repr(r3))
+    print('  if r3... %s'%repr(bool(r3)))
+    print('  r3.area() = %d'%r3.area())
+
+    r4 = Rect(Point(0,0),Point(0,1))
+    print('r4 = %s'%repr(r4))
+    print('  if r4... %s'%repr(bool(r4)))
+    print('  r4.area() = %d'%r4.area())
+
+    r5 = Rect(Point(0,0),Point(1,1))
+    print('r5 = %s'%repr(r5))
+    print('  if r5... %s'%repr(bool(r5)))
+    print('  r5.area() = %d'%r5.area())
